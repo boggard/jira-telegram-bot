35ef19960a9238bfeb85db7ffc5bda54e11eb71b
boggard <varyanik2@gmail.com>
2017-10-05 10:47:11 +0300

    get with default value ofr issue fields

--- dto/issue.py
+++ dto/issue.py
@@ -5,17 +5,17 @@ from utils.markdown import markdown_prepare
 
 class Issue:
     def __init__(self, jira_issue, comments):
-        self.created = date_util.to_str(date_util.format_jira_date(jira_issue["fields"]["created"]))
-        self.updated = date_util.to_str(date_util.format_jira_date(jira_issue["fields"]["updated"]))
-        self.caption = jira_issue["fields"]["summary"]
-        self.project_name = jira_issue["fields"]["project"]["name"]
+        self.created = date_util.to_str(date_util.format_jira_date(jira_issue["fields"].get("created")))
+        self.updated = date_util.to_str(date_util.format_jira_date(jira_issue["fields"].get("updated")))
+        self.caption = jira_issue["fields"].get("summary")
+        self.project_name = jira_issue["fields"]["project"].get("name")
         self.alias = jira_issue["key"]
         self.link = JIRA_WEB_URL + "browse/" + jira_issue["key"]
-        self.description = jira_issue["fields"]["description"]
-        self.author = jira_issue["fields"]["reporter"]["displayName"]
-        assignee = jira_issue["fields"]["assignee"]
+        self.description = jira_issue["fields"].get("description", default="")
+        self.author = jira_issue["fields"]["reporter"].get("displayName")
+        assignee = jira_issue["fields"].get("assignee")
         self.assignee = assignee["displayName"] if assignee is not None else "Not defined"
-        self.status = jira_issue["fields"]["status"]["name"]
+        self.status = jira_issue["fields"]["status"].get("name", default="")
         self.components = ",".join([component["name"] for component in jira_issue["fields"]["components"]])
         self.comments = comments
 
